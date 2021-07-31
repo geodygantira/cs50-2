e0c7f66db6297da4060c4d6e62b5fc05d7eb2f7c
geodygantira <geodygantira@gmail.com>
2021-07-31 16:58:39 +0700

    readibility done!

--- Week 2/Readability.c
+++ Week 2/Readability.c
@@ -2,64 +2,111 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-//letters : count the number of uppercase and lowercase letters in a text
-  //words : count the number of words in a sentence, a word is any sequence of characthers separated by space
+#include <math.h>
 
-
+//Variable Declarations
 int n;
 int i;
-int l;
-int w;
+int x;
+int w = 1;
+int z;
+float L;
+float S;
+int grade;
+
+//function declarations
 int letters();
 int words();
+int sentence();
+int compute_grade();
+
+
 int main () {
 
   char text[10];   /* actual variable declaration */
   char  *ch;        /* pointer variable declaration */
 
-  ch = (char*)calloc(n, sizeof(char));
-  //ch = text;  /* store address of var in pointer variable*/
+  ch = (char*)malloc(1000*sizeof(char)); /* dynamic allocation of text array in ch pointer*/
 
+  //get text from user
   printf("Text :");
-  scanf("%s", ch);
+  gets(ch);
+  //compute n, length of array 
   n = strlen(ch);
-  printf("textnya adalah : %s \n", ch);
-  printf("Value of *ip variable: %c\n", ch[0]);
 
   letters(ch);
   words(ch);
+  sentence(ch);
+  compute_grade(ch);
    
   return 0;
 }
 
+//function to compute the number of letters
 int letters (char word[])
 {
-  for (i =0, n = strlen(word); i <=n; i++)
+  for (i =0; i <=n; i++)
   {
+    //function to check is word[i] an alphabet or not
     if (isalpha(word[i]))
     {
-      l++;
-    }
-    else if (word[i] == 20)
-    {
-      i++;
+      x++;
     }
   }
-  printf("letters : %i \n", l);
-  return l;
+  return x;
 }
 
+//function to compute the number of sentence
 int words (char word[])
 {
-  for (i =0, n =strlen(word); i <=n; i++)
+  for (i =0; i <=n; i++)
   {
+    //function to check is word[i] a white space or not
     if (isspace(word[i]))
     {
       w++;
     }
     else 0;
   }
-  printf("words : %i \n", w);
   return w;
 }
 
+//function to count the number of sentence
+int sentence (char word[])
+{
+  for (i =0; i <=n; i++)
+  {
+    //function to check if word[i] is dot(.), exclamation point(!), or question mark(?)
+    if (word[i] ==33 || word[i] == 46 || word[i] == 63)
+    {
+      z++;
+    }
+    else 0;
+  }
+  return z;
+}
+
+//function to compute grade
+int compute_grade(char word[])
+{
+  //function to compute L & S, first cast the data type into float for more accurate division
+  L = ((float) x/ (float) w)*100;
+  S = ((float) z/ (float) w)*100;
+
+
+  grade = (0.0588*L)-(0.296*S)-15.8;
+  round(grade);
+  if (grade < 1)
+  {
+    printf("Before grade 1");
+  }
+  else if (grade > 16)
+  {
+    printf("Grade 16+");
+  }
+  else 
+  {
+    printf("grade : %i ", grade);
+  }
+  return grade;
+}
