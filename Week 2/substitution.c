148558e433e34bcb5cb45c1ff8a30a81939ec828
geodygantira <geodygantira@gmail.com>
2021-08-01 18:04:23 +0700

    substitution belum selesai

---
+++ Week 2/substitution.c
@@ -0,0 +1,86 @@
+#include <stdio.h>  
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+//Variable declaration
+int i;
+int n;
+int k;
+char text[100];
+
+//pointer declaration and assignment
+char *ch = text;
+
+//function declaration
+char get_text();
+char cipher();
+
+int main(int argc, char *argv[] ) 
+{  
+    //return 1 if argument is less or more than 2
+   if(argc < 2 || argc >2)
+   {  
+      printf(".\substituion key \n");
+      return 1;  
+   }
+   //check if every character is digit, return 1 if not digit.
+   else if (strlen(argv[1]) < 26)
+   {
+       printf("key must contain 26 character");
+       return 1;
+   }
+   else 
+   {
+    for (i=0, n=strlen(argv[1]); i<n; i++)
+    {
+       if (isalpha(argv[1][i]))
+       {
+           printf("char: %c \n", argv[1][i]);
+       }
+       else 
+       {
+        printf("not alphabet\n");
+        return 1;
+       }
+    }
+   }
+   get_text();
+   cipher();
+}  
+
+//get text from user
+char get_text()
+{
+    printf("plaintext :");
+    gets(ch);
+    printf("text is : %s \n", ch);
+}
+
+//function to convert text
+char cipher()
+{
+
+    for (i =0, n=strlen(ch); i<=n; i++)
+    {
+        //check if ch[i] is an alphabet
+        if(isalpha(ch[i]));
+        {
+            if(islower(ch[i]))
+            {
+                ch[i] = ch[i] - 97;
+                //%26 to make the function is wrap around function, if Z can back to A
+                ch[i] = (ch[i] + k) %26;
+                ch[i] = ch[i] + 97;
+            }
+            else if (isupper(ch[i]))
+            {
+                ch[i] = ch[i] - 65;
+                ch[i] = (ch[i] + k) %26;
+                ch[i] = ch[i] + 65;
+            }
+            
+        }
+    }
+    printf("cipher text : %s \n", ch);   
+}
